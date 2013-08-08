1e0c0128df35f50f3bfdb8d002129b6b17a6a226
lilo86 <viip.inheart@gmail.com>
2013-08-08 16:28:18 +0700

    Create vi.js
    Vietnam language

---
+++ Language/locale/vi.js
@@ -0,0 +1,37 @@
+/* Vietnam LANGUAGE 
+================================================== */
+if(typeof VMM != 'undefined') {
+  VMM.Language = {
+		lang: "vi",
+		api: {
+			wikipedia: "vi"
+		},
+		date: {
+			month: ["Tháng 1", "Tháng 2", "Tháng 3", "Tháng 4", "Tháng 5", "Tháng 6", "Tháng 7", "Tháng 8", "Tháng 9", "Tháng 10", "Tháng 11", "Tháng 12"],
+			month_abbr: ["Th1.", "Th2.", "Th3.", "Th4.", "Th5.", "Th6.", "Th7.", "Th8.", "Th9.", "Th10.", "Th11.", "Th12."],
+			day: ["Chủ nhật","Thứ 2", "Thứ 3", "Thứ 4", "Thứ 5", "Thứ 6", "Thứ 7"],
+			day_abbr: ["CN.","T2.", "T3.", "T4.", "T5.", "T6.", "T7."]
+		}, 
+		dateformats: {
+			year: "yyyy",
+			month_short: "mmm",
+			month: "mmmm yyyy",
+			full_short: "mmm d",
+			full: "mmmm d',' yyyy",
+			time_short: "h:MM:ss TT",
+			time_no_seconds_short: "h:MM TT",
+			time_no_seconds_small_date: "h:MM TT'<br/><small>'mmmm d',' yyyy'</small>'",
+			full_long: "mmm d',' yyyy 'at' h:MM TT",
+			full_long_small_date: "h:MM TT'<br/><small>mmm d',' yyyy'</small>'"
+		},
+		messages: {
+			loading_timeline: "Đang tải... ",
+			return_to_title: "Trở lại tiêu đề
+			expand_timeline: "Mở rộng Timeline",
+			contract_timeline: "Thu hẹp Timeline",
+			wikipedia: "Từ Bách Khoa toàn thư Wikipedia",
+			loading_content: "Tải nội dung",
+			loading: "Đang tải"
+		}
+	}
+}
